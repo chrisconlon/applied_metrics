c4e3a5753f11fc62af85969d62b46965e8528601
Chris Conlon <christopherconlon@gmail.com>
2025-10-21 11:40:25 -0400

    Update iv_code.R

--- L8 - Instrumental Variables/iv_code.R
+++ L8 - Instrumental Variables/iv_code.R
@@ -4,10 +4,10 @@ babydata <- data.frame(AE98) %>% mutate(BoyBoy = 1* (boy1st ==1 & boy2nd==1),
 m5= feols(worked ~ morekids + black + hisp +othrace + age + agefst |
         samesex ~ boy1st + boy2nd, data=babydata)
 
-flm1 = feols(morekids ~ samesex + black + hisp, data=babydata)
+flm1 = feols(morekids ~ samesex + black + hisp + othrace, data=babydata)
 lh1 = linearHypothesis(flm1,"samesex = 0", test="F")
 
 # First Stage Test in AE w/ Multiple Instruments
-flm2 = feols(morekids ~ BoyBoy + GirlGirl + black + hisp, data=babydata)
+flm2 = feols(morekids ~ BoyBoy + GirlGirl + black + hisp + othrace, data=babydata)
 lh2 = linearHypothesis(flm2,c("BoyBoy","GirlGirl"), test="F")
 
